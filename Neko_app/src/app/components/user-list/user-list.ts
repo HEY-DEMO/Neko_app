17a2ad9335a2a40cda5b48f8fecff62b33cdaffd
Tharun <kodurutharun18@gmail.com>
2025-10-10 12:19:24 +0530

    created homepage containing some website info and a navbar

--- Neko_app/src/app/components/user-list/user-list.ts
+++ Neko_app/src/app/components/user-list/user-list.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { UserService, User } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-list',
-  templateUrl: './user-list.html'
+  templateUrl: './user-list.html',
+  imports: [CommonModule],
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
