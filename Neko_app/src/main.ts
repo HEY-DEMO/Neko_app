28f33efebe6acbe612cfa1815875e5077a4058a9
Tharun <kodurutharun18@gmail.com>
2025-10-12 14:00:03 +0530

    modified signup page for validations connect signup page to register user using web api. modified main.ts file according to the config file modifed home.html to use routerlinks instead of 'href' modified usercontroller.cs to validate if the user exists or not

--- Neko_app/src/main.ts
+++ Neko_app/src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err:any) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule)
+  ]
+}).catch((err:any) => console.error(err));
